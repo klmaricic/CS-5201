ee988530a80cc077df5af063934a7c7aef785a05
Kelsey Maricic <klmhw9@mst.edu>
2015-02-10 00:14:51 -0600

    Added more functions

--- Hw2/CylindricalCoord.h
+++ Hw2/CylindricalCoord.h
@@ -4,42 +4,68 @@
 /// @brief Write a templated class to implement cylindrical coordinates
 //////////////////////////////////////////////////////////////////////
 
+#ifndef __CylindricalCoord_H_INCLUDED
+#define __CylindricalCoord_H_INCLUDED
+#include <stdexcept>
+#include <cmath>
+
 template<class T>
 class CylindricalCoord
 {
   public:
+    /************************** Constructors/Destructor  **************************/
     CylindricalCoord<T>::CylindricalCoord(): m_r(0), m_theta(0), m_z(0) {}
 	CylindricalCoord<T>::CylindricalCoord(const T& r, const T& theta, const T& z): m_r(r), m_theta(theta), m_z(z) {}
 	CylindricalCoord<T>::CylindricalCoord(const CylindricalCoord<T> &coord): m_r(coord.m_r), m_theta(coord.m_theta), m_z(coord.m_z) {}
 	~CylindricalCoord() {}
 	
-	/**************************Operators**************************/
+	/************************** Operators **************************/
 	/* Purpose:	Subtraction operator
 	   Pre: 	None
 	   Post:	Returns the reflection through the origin or pole
 	*/
-	CylindricalCoord<T> CylindricalCoord<T>::operator-(const CylindricalCoord<T>& b) const;
+	CylindricalCoord<T> CylindricalCoord<T>::operator-() const
 	
 	/* Purpose:	Assignment operator
 	   Pre:		None
 	   Post:	Copies b onto the calling object
 	*/
-	T CylindricalCoord<T>::operator=(const CylindricalCoord<T>& b);
+	T CylindricalCoord<T>::operator=() const
 	
 	/* Purpose:	Equal to operator
 	   Pre: 	None
-	   Post:	Returns if b has the same values as the calling object or not
+	   Post:	Returns true if b has the same values as the calling object. Returns false otherwise.
 	*/
 	bool CylindricalCoord<T>::operator==(const CylindricalCoord<T>& b) const;
 	
-	/* Purpose:
-	   Pre:
-	   Post:
+	/* Purpose:	Not equal to operator
+	   Pre: 	None
+	   Post: 	Returns true if b does not have all of the same values as the calling object. Returns false otherwise.
 	*/
 	bool CylindricalCoord<T>::operator!=(const CylindricalCoord<T>& b) const
+	
+	/* Purpose:	Greater than operator
+	   Pre: 	None
+	   Post: 	Returns true if the calling object is farther from the origin than b. Returns false otherwise.
+	*/
+	CylindricalCoord<T> operator>(const CylindricalCoord<T>& b) const
+	
+	/* Purpose:	Less than operator
+	   Pre:		None
+	   Post:	Returns true if the calling object is closer to the origin than b. Returns false otherwise.
+	*/
+	CylindricalCoord<T> operator<(const CylindricalCoord<T>& b) const
+	
+	/* Purpose:	Converts the cylindrical coordinates to cartesian values
+	   Pre:		None
+	   Post:	Returns a string that represents the converted cartesian values
+	*/
+	string CylindricalCoord<T>::cartesianCoord(const CylindricalCoord<T>& b) const
 	
   private:
     T m_r;		//Radial distance
 	T m_theta;	//Angle	
 	T m_z;		//Height
 };
+#include "CylindricalCoord.hpp"
+#endif
--- Hw2/CylindricalCoord.hpp
+++ Hw2/CylindricalCoord.hpp
@@ -5,19 +5,25 @@ template <class T>
 /// @brief Write a templated class to implement cylindrical coordinates
 //////////////////////////////////////////////////////////////////////
 
-CylindricalCoord<T> CylindricalCoord<T>::operator-(const CylindricalCoord<T>& b) const
+CylindricalCoord<T> CylindricalCoord<T>::operator-() const
 {
-
+  return CylindricalCoord<T>(m_r, -m_theta, -m_z);
 }
 
 template <class T>
 T CylindricalCoord<T>::operator~(const CylindricalCoord<T>& b) const
 {
-  return sqrt(pow(b.m_r,2)+pow(b.m_theta,2)+pow(b.m_z,2));
+  return sqrt(pow(b.m_r,2)+pow(b.m_z,2));
+}
+
+template<class T>
+CylindricalCoord<T> CylindricalCoord<T>::operator!() const
+{
+
 }
 
 template <class T>
-T CylindricalCoord<T>::operator=(const CylindricalCoord<T>& b)
+T CylindricalCoord<T>::operator=() const
 {
   m_r = b.m_r;
   m_theta = b.m_theta;
@@ -37,6 +43,24 @@ bool CylindricalCoord<T>::operator!=(const CylindricalCoord<T>& b) const
   return !((m_r == b.m_r)&&(m_theta == b.m_theta)&&(m_z == b.m_z));
 }
 
+bool operator<(const CylindricalCoord<T>& b) const
+{
+  T mag1, mag2;
+  mag1 = ~*this;
+  mag2 = ~b;
+  
+  return mag1 < mag2;
+}
+
+CylindricalCoord<T> operator>(const CylindricalCoord<T>& b) const
+{
+  T mag1, mag2;
+  mag1 = ~*this;
+  mag2 = ~b;
+  
+  return mag1 > mag2;
+}
+
 template <class T>
 string CylindricalCoord<T>::cartesianCoord(const CylindricalCoord<T>& b) const
 {
@@ -44,6 +68,4 @@ string CylindricalCoord<T>::cartesianCoord(const CylindricalCoord<T>& b) const
   stream<<"("<<(b.m_r*cos(theta))<<","<<b.m_r*sin(theta))<<","<<b.m_r<<")";
   string cartCoord = stream.str();
   return cartCoord;
-  //x=rcos(theta)
-  //y=rsin(theta)
 }
